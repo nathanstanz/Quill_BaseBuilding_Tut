d454db97d1cb177ea6a83206a9c82e56358330b6
Nathan Stanz <nathanstanz@Nathans-MacBook-Pro.local>
2017-03-18 19:55:09 -0700

    Adding assets and scripts

---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+
+    public enum TileType { Empty, Floor };
+
+    TileType type = TileType.Empty;
+
+    public TileType Type {
+        get {
+            return type;
+        }
+        set {
+            type = value;
+            // Call the callback and let things know we have changed
+        }
+    }
+
+    LooseObject looseObject;
+    InstalledObject installedObject;
+
+    World world;
+    int x;
+
+    public int X {
+        get {
+            return x;
+        }
+    }
+
+    int y;
+
+    public int Y {
+        get {
+            return y;
+        }
+    }
+
+    public Tile( World world, int x, int y ) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
